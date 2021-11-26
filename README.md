77f705bebc5b3103b1238ab865d1532f613b7f20
shseok <gus0674@ajou.ac.kr>
2021-11-26 21:23:43 +0900

    Finish HTML Template

---
+++ README.md
@@ -0,0 +1,11 @@
+# E-Commerce 🛒
+
+# Library
+1. j-Query
+2. owl-carousel
+3. bootstrap
+4. font-awesome
+5. isotope (j Query Plugin)
+
+# sass
+- npm start
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         items: 1
     });
 
-    // 아이템
+    // 상위 아이템
     $("#top-sale .owl-carousel").owlCarousel({
         loop: true,
         nav: true, // 밑에 화살표
@@ -23,4 +23,48 @@ $(document).ready(function () {
             }
         }
     });
+
+    // isotope filer
+    let $grid = $(".grid").isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    });
+
+    $(".button-group").on("click", "button", function () {
+        let filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    })
+
+    // 새로운 아이템
+    $("#new-items .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,// 밑에 점
+        responsive: { // px 단위 아이템 개수
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+    // 블로그 owl
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    })
 });
