d4d1d4a97ca844b8e16874e060127f3ce83365c0
shseok <gus0674@ajou.ac.kr>
2021-11-28 15:05:03 +0900

    Update Fetching Data

--- Template/_products.php
+++ Template/_products.php
@@ -1,132 +1,143 @@
-<section id="product" class="py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6">
-                <img src="./assets/products/1.png" alt="img-fluid" class="img-fluid">
-                <div class="form-row pt-4 font-size-16 font-baloo">
-                    <div class="col">
-                        <button type="submit" class="btn btn-danger form-control">바로 구매</button>
-                    </div>
-                    <div class="col">
-                        <button type="submit" class="btn btn-warning form-control">장바구니 담기</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 py-5">
-                <h5 class="font-baloo font-size-20">Samsung Galaxy 10</h5>
-                <small>by Samsung</small>
-                <div class="d-flex">
-                    <div class="rating text-warning font-size-12">
-                        <span><i class="fas fa-star"></i></span>
-                        <span><i class="fas fa-star"></i></span>
-                        <span><i class="fas fa-star"></i></span>
-                        <span><i class="fas fa-star"></i></span>
-                        <span><i class="far fa-star"></i></span>
-                    </div>
-                    <a href="#" class="px-2 font-baloo font-size-14">20,534 ratings | 1000+ answered
-                        questions</a>
-                </div>
-                <hr class="m-0">
-                <!-- 가격 -->
-                <table class="my-3">
-                    <tr class="font-baloo font-size-14">
-                        <td>M.R.P</td>
-                        <td><strike>1,309,000원</strike></td>
-                    </tr>
-                    <tr class="font-baloo font-size-14">
-                        <td>거래금액</td>
-                        <td class="font-size-20 text-danger">
-                            <span>1,200,000</span>원<small class="font-size-12 text-dark">&nbsp;&nbsp;inclusive
-                                of all
-                                taxes</small>
-                        </td>
-                    </tr>
-                    <tr class="font-baloo font-size-14">
-                        <td>할인금액</td>
-                        <td>
-                            <span class="font-size-16 text-danger">109,000원</span>
-                        </td>
-                    </tr>
-                </table>
-                <div class="policy">
-                    <div class="d-flex">
-                        <div class="return text-center mr-5">
-                            <div class="font-size-20 my-2 color-second">
-                                <span class="fas fa-retweet border p-3 rounded-pill"></span>
-                            </div>
-                            <a href="#" class="font-baloo font-size-12">10 Days<br> Replacement</a>
-                        </div>
-                        <div class="return text-center mr-5">
-                            <div class="font-size-20 my-2 color-second">
-                                <span class="fas fa-truck border p-3 rounded-pill"></span>
-                            </div>
-                            <a href="#" class="font-baloo font-size-12">Daily Tuition<br> Deliverd</a>
-                        </div>
-                        <div class="return text-center mr-5">
-                            <div class="font-size-20 my-2 color-second">
-                                <span class="fas fa-check-double border p-3 rounded-pill"></span>
-                            </div>
-                            <a href="#" class="font-baloo font-size-12">1 Year<br> Warranty</a>
-                        </div>
-                    </div>
-                </div>
-                <hr>
-                <div id="order-details" class="font-baloo d-flex flex-column text-dark">
-                    <small>12월 1일까지 배달</small>
-                    <small>Sold by <a href="#">Daily Electronics</a>(4.5 out of 5 | 18,198 ratings)</small>
-                    <small><i class="fas fa-map-marker-alt color-primary"></i>&nbsp;&nbsp;Deliver to
-                        Customer - 424201</small>
-                </div>
+<?php
+$item_id = $_GET['item_id'] ?? 1;
+foreach ($product->getData() as $item) :
+    if ($item['item_id'] == $item_id) :
+?>
+
+        <section id="product" class="py-3">
+            <div class="container">
                 <div class="row">
-                    <div class="col-6">
-                        <div class="color my-3">
-                            <div class="d-flex justify-content-between">
-                                <h6 class="font-baloo">색상:</h6>
-                                <div class="p-2 color-yellow-bg rounded-circle"><button class="btn font-size-14"></button></div>
-                                <div class="p-2 color-primary-bg rounded-circle"><button class="btn font-size-14"></button></div>
-                                <div class="p-2 color-second-bg rounded-circle"><button class="btn font-size-14"></button></div>
+                    <div class="col-sm-6">
+                        <img src="<?php echo $item['item_image'] ?? "./assets/products/1.png" ?>" alt="img-fluid" class="img-fluid">
+                        <div class="form-row pt-4 font-size-16 font-baloo">
+                            <div class="col">
+                                <button type="submit" class="btn btn-danger form-control">바로 구매</button>
+                            </div>
+                            <div class="col">
+                                <button type="submit" class="btn btn-warning form-control">장바구니 담기</button>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6">
-                        <div class="qty d-flex">
-                            <h6 class="font-baloo" style="width: 60px; margin: 0">수량:</h6>
-                            <div class="px-4 d-flex font-baloo">
-                                <button class="qty-up border bg-light" data-id="pro1"><i class="fas fa-angle-up"></i></button>
-                                <input type="text" data-id="pro1" class="qty_input border px-2 w-50 bg-light" disabled value="1" placeholder="1">
-                                <button class="qty-down border bg-light" data-id="pro1"><i class="fas fa-angle-down"></i></button>
+                    <div class="col-sm-6 py-5">
+                        <h5 class="font-baloo font-size-20"><?php echo $item['item_name'] ?? "Unkown"; ?></h5>
+                        <small>by <?php echo $item['item_brand'] ?? "Brand"; ?></small>
+                        <div class="d-flex">
+                            <div class="rating text-warning font-size-12">
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="far fa-star"></i></span>
+                            </div>
+                            <a href="#" class="px-2 font-baloo font-size-14">20,534 ratings | 1000+ answered
+                                questions</a>
+                        </div>
+                        <hr class="m-0">
+                        <!-- 가격 -->
+                        <table class="my-3">
+                            <tr class="font-baloo font-size-14">
+                                <td>M.R.P</td>
+                                <td>$<strike><?php echo $item['item_price'] ?? 0; ?></strike></td>
+                            </tr>
+                            <tr class="font-baloo font-size-14">
+                                <td>거래금액</td>
+                                <td class="font-size-20 text-danger">
+                                    <span>1,200,000</span>원<small class="font-size-12 text-dark">&nbsp;&nbsp;inclusive
+                                        of all
+                                        taxes</small>
+                                </td>
+                            </tr>
+                            <tr class="font-baloo font-size-14">
+                                <td>할인금액</td>
+                                <td>
+                                    <span class="font-size-16 text-danger">109,000원</span>
+                                </td>
+                            </tr>
+                        </table>
+                        <div class="policy">
+                            <div class="d-flex">
+                                <div class="return text-center mr-5">
+                                    <div class="font-size-20 my-2 color-second">
+                                        <span class="fas fa-retweet border p-3 rounded-pill"></span>
+                                    </div>
+                                    <a href="#" class="font-baloo font-size-12">10 Days<br> Replacement</a>
+                                </div>
+                                <div class="return text-center mr-5">
+                                    <div class="font-size-20 my-2 color-second">
+                                        <span class="fas fa-truck border p-3 rounded-pill"></span>
+                                    </div>
+                                    <a href="#" class="font-baloo font-size-12">Daily Tuition<br> Deliverd</a>
+                                </div>
+                                <div class="return text-center mr-5">
+                                    <div class="font-size-20 my-2 color-second">
+                                        <span class="fas fa-check-double border p-3 rounded-pill"></span>
+                                    </div>
+                                    <a href="#" class="font-baloo font-size-12">1 Year<br> Warranty</a>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <div id="order-details" class="font-baloo d-flex flex-column text-dark">
+                            <small>12월 1일까지 배달</small>
+                            <small>Sold by <a href="#">Daily Electronics</a>(4.5 out of 5 | 18,198 ratings)</small>
+                            <small><i class="fas fa-map-marker-alt color-primary"></i>&nbsp;&nbsp;Deliver to
+                                Customer - 424201</small>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="color my-3">
+                                    <div class="d-flex justify-content-between">
+                                        <h6 class="font-baloo">색상:</h6>
+                                        <div class="p-2 color-yellow-bg rounded-circle"><button class="btn font-size-14"></button></div>
+                                        <div class="p-2 color-primary-bg rounded-circle"><button class="btn font-size-14"></button></div>
+                                        <div class="p-2 color-second-bg rounded-circle"><button class="btn font-size-14"></button></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="qty d-flex">
+                                    <h6 class="font-baloo" style="width: 60px; margin: 0">수량:</h6>
+                                    <div class="px-4 d-flex font-baloo">
+                                        <button class="qty-up border bg-light" data-id="pro1"><i class="fas fa-angle-up"></i></button>
+                                        <input type="text" data-id="pro1" class="qty_input border px-2 w-50 bg-light" disabled value="1" placeholder="1">
+                                        <button class="qty-down border bg-light" data-id="pro1"><i class="fas fa-angle-down"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="size my-3">
+                            <div class="d-flex justify-content-between w-75">
+                                <h6 class="font-baloo">크기:</h6>
+                                <div class="font-baloo border p-2">
+                                    <button class="btn p-0 font-size-14">4GM RAM</button>
+                                </div>
+                                <div class="font-baloo border p-2">
+                                    <button class="btn p-0 font-size-14">6GM RAM</button>
+                                </div>
+                                <div class="font-baloo border p-2">
+                                    <button class="btn p-0 font-size-14">8GM RAM</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="size my-3">
-                    <div class="d-flex justify-content-between w-75">
-                        <h6 class="font-baloo">크기:</h6>
-                        <div class="font-baloo border p-2">
-                            <button class="btn p-0 font-size-14">4GM RAM</button>
-                        </div>
-                        <div class="font-baloo border p-2">
-                            <button class="btn p-0 font-size-14">6GM RAM</button>
-                        </div>
-                        <div class="font-baloo border p-2">
-                            <button class="btn p-0 font-size-14">8GM RAM</button>
-                        </div>
+                    <!-- full -->
+                    <div class="col-12">
+                        <h6 class="font-baloo">제품 설명</h6>
+                        <hr>
+                        <p class="font-baloo font-size-14">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Explicabo debitis nesciunt atque laborum nemo voluptatem quisquam voluptatibus, doloremque
+                            delectus voluptates consequuntur possimus similique aliquid cum fugiat rem saepe cupiditate
+                            fuga.</p>
+                        <p class="font-baloo font-size-14">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Explicabo debitis nesciunt atque laborum nemo voluptatem quisquam voluptatibus, doloremque
+                            delectus voluptates consequuntur possimus similique aliquid cum fugiat rem saepe cupiditate
+                            fuga.</p>
                     </div>
                 </div>
             </div>
-            <!-- full -->
-            <div class="col-12">
-                <h6 class="font-baloo">제품 설명</h6>
-                <hr>
-                <p class="font-baloo font-size-14">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Explicabo debitis nesciunt atque laborum nemo voluptatem quisquam voluptatibus, doloremque
-                    delectus voluptates consequuntur possimus similique aliquid cum fugiat rem saepe cupiditate
-                    fuga.</p>
-                <p class="font-baloo font-size-14">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Explicabo debitis nesciunt atque laborum nemo voluptatem quisquam voluptatibus, doloremque
-                    delectus voluptates consequuntur possimus similique aliquid cum fugiat rem saepe cupiditate
-                    fuga.</p>
-            </div>
-        </div>
-    </div>
-</section>
+        </section>
+
+<?php
+    endif;
+endforeach;
+?>
